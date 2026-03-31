6b9e0029a03899e4e66e457f07b2e9dbc3927ff7
meeten <153901120+meeten@users.noreply.github.com>
2026-03-31 20:36:32 +0200

    Setting entry point for Hilt in :app module

--- app/src/main/java/com/example/starwarsapp/MainActivity.kt
+++ app/src/main/java/com/example/starwarsapp/MainActivity.kt
@@ -4,14 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.designsystem.theme.StarWarsAppTheme
+import com.example.home.HomeScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             StarWarsAppTheme {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    HomeScreen()
+                }
             }
         }
     }
