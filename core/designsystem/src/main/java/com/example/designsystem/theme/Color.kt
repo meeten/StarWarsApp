b67bfca4a3e8eb8d774ad35a11f671a1ca1b7f2a
meeten <153901120+meeten@users.noreply.github.com>
2026-03-31 17:06:45 +0200

    Update Color.kt

--- core/designsystem/src/main/java/com/example/designsystem/theme/Color.kt
+++ core/designsystem/src/main/java/com/example/designsystem/theme/Color.kt
@@ -2,5 +2,5 @@ package com.example.designsystem.theme
 
 import androidx.compose.ui.graphics.Color
 
-val lightBlue = Color(0xFF8692B6)
+val lightBlue = Color(0xFF536698)
 val lightLavenderGray = Color(0xFFE1E2EC)
